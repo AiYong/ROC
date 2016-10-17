d041b54f747df5eddfbedf9128a3f3169baeca29
aiyong <aiyong@localhost.localdomain>
2016-10-17 23:15:15 +0800

    override function for AClassASTVisitor

--- AClassDefinition.cpp
+++ AClassDefinition.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "AClassDefinition.h"
+
+
+std::list<AFieldDefinition *> const& AClassDefinition::GetFieldDefinitions()
+{
+    return FieldDefinitions;
+}
+
+std::list<AMethodDefinition *> const& AClassDefinition::GetMethodDefinitions()
+{
+    return MethodDefinitions;
+}
+
+std::list<AAnnotationDefinition *> const& AClassDefinition::GetAnnotationDifinitions()
+{
+    return AnnotationDefinitions;
+}
--- AClassDefinition.h
+++ AClassDefinition.h
@@ -6,13 +6,43 @@
 #define ROC_ACLASSDEFINITION_H
 
 #include "AFieldDefinition.h"
-#include "AMethodGenerator.h"
+#include "AMethodDefinition.h"
 #include "AAnnotationDefinition.h"
 
+#include <list>
+
+/**
+ *
+ */
 class AClassDefinition
 {
 public:
 
+    /**
+     *
+     * @return
+     */
+    std::list<AFieldDefinition*> const& GetFieldDefinitions();
+
+    /**
+     *
+     * @return
+     */
+    std::list<AMethodDefinition*> const& GetMethodDefinitions();
+
+    /**
+     *
+     * @return
+     */
+    std::list<AAnnotationDefinition*> const& GetAnnotationDifinitions();
+
+private:
+
+    friend class AClassParser;
+
+    std::list<AFieldDefinition*> FieldDefinitions;
+    std::list<AMethodDefinition*> MethodDefinitions;
+    std::list<AAnnotationDefinition*> AnnotationDefinitions;
 
 };
 
